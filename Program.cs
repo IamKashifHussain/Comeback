773ee66363c6263e6c31c2a54be440805d07608f
Kashif Hussain <kashifhussainkhan123@gmail.com>
2025-11-15 14:12:26 +0500

    adding to the git

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+float a = 5000000000;
+int b = 10;
+
+float sum = a + b;
+Console.WriteLine($"The sum of {a} and {b} is {sum}.");
